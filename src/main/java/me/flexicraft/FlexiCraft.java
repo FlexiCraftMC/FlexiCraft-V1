db323c07954255b3b1eb2ba31a7bcace467124ab
OlliesWorld <93611253+OlliesGitHubWorld@users.noreply.github.com>
2024-06-08 22:13:44 +0200

    Join & Leave message
    Fixed Join and Leave message and created a config file

--- src/main/java/me/flexicraft/FlexiCraft.java
+++ src/main/java/me/flexicraft/FlexiCraft.java
@@ -1,24 +1,18 @@
 package me.flexicraft;
 
+import me.flexicraft.listeners.JoinLeaveListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FlexiCraft  extends JavaPlugin {
-    @Override
-    public void onLoad() {
-        // 1.
-        // This method will be called when the plugin is being loaded, but not yet enabled.
-    }
 
     @Override
     public void onEnable() {
-        // 2.
-        // This method will be called when the plugin is enabled.
-        getLogger().info("Plugin has been enabled");
-    }
+        // Register event Listener
+        JoinLeaveListener joinLeaveListener = new JoinLeaveListener(this);
+        getServer().getPluginManager().registerEvents(joinLeaveListener, this);
 
-    @Override
-    public void onDisable() {
-        // 3.
-        // This method will be called when the server shuts down.
+        // Load config.yml
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
     }
 }
--- src/main/java/me/flexicraft/listeners/JoinLeaveListener.java
+++ src/main/java/me/flexicraft/listeners/JoinLeaveListener.java
@@ -1,4 +1,36 @@
 package me.flexicraft.listeners;
 
-public class JoinLeaveListener {
+import me.flexicraft.FlexiCraft;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinLeaveListener implements Listener {
+    private FlexiCraft plugin;
+
+    public JoinLeaveListener(FlexiCraft plugin) { this.plugin = plugin; }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+        // Gets the player from the event
+        final Player player = event.getPlayer();
+
+        if (player.hasPlayedBefore()){
+            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("join-message").replace("%player%", player.getDisplayName())));
+        }else{
+            event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("first-time-join-message").replace("%player%", player.getDisplayName())));
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event){
+        // Gets the player from the event
+        final Player player = event.getPlayer();
+
+        event.setQuitMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("leave-message").replace("%player%", player.getDisplayName())));
+    }
 }
